404adbcc146c369ad637865a679142a9166ad65a
奈良亮 <motorbicykle@gmail.com>
2020-03-16 13:06:16 +0900

    add initialize to gmail.rb

--- gmail.rb
+++ gmail.rb
@@ -2,16 +2,20 @@ require "json"
 require "mail"
 
 class Gmail
-  def initialize
+  def initialize(**mail)
+    @from = mail[:from]
+    @passwd = mail[:passwd]
+    @to = mail[:to]
+    @subject = mail[:subject]
 
   end
 
   def deliver(weather_response)
-    mail_from   = 'yuyueureka157@gmail.com'
-    mail_passwd = 'udxieczjeuiprybt'
-    mail_to     = 'yuyueureka157@gmail.com'
-    mail_subject= 'メールタイトル'
-    mail_body   = <<~EOS
+    #mail_from   = 'yuyueureka157@gmail.com'
+    #mail_passwd = 'udxieczjeuiprybt'
+    #mail_to     = 'yuyueureka157@gmail.com'
+    #mail_subject= 'メールタイトル'
+    mail_body = <<~EOS
       メール本文
       本日の#{ENV["PREFECTURE"]}の天気：#{weather_response}
     EOS
@@ -21,20 +25,18 @@ class Gmail
         :address => 'smtp.gmail.com',
         :port => 587,
         :domain => 'example.com',
-        :user_name => "#{mail_from}",
-        :password => "#{mail_passwd}",
+        :user_name => "#{@from}",
+        :password => "#{@passwd}",
         :authentication => :login,
         :enable_starttls_auto => true
       }
     end
 
     mail = Mail.new do
-      from "#{mail_from}"
-      to "#{mail_to}"
-      subject "#{mail_subject}"
-      body <<~EOS
-        #{mail_body}
-      EOS
+      from "#{@from}"
+      to "#{@to}"
+      subject "#{@subjecr}"
+      body "#{mail_dody}"
     end
 
     mail.charset = "UTF-8"
--- main.rb
+++ main.rb
@@ -2,5 +2,5 @@ require "./weather_api"
 require "./gmail"
 
 weather_response = Weather.request_api
-gmail = Gmail.new
+gmail = Gmail.new(to:'yuyueureka157@gmail.com', passwd:'udxieczjeuiprybt', to:'yuyueureka157@gmail.com', subject:'メールタイトル' )
 gmail.deliver(weather_response)
